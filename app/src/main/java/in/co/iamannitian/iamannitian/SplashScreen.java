2c24ef608c9a51ed5100e0c1763c9e61147b34ef
BlackHatos <pnssoftwares7@gmail.com>
2020-04-16 13:47:16 +0530

    Initial commit

---
+++ app/src/main/java/in/co/iamannitian/iamannitian/SplashScreen.java
@@ -0,0 +1,35 @@
+/*
+* @Project I Am An Nitian
+* @Date 16 April 2020
+ */
+
+package in.co.iamannitian.iamannitian;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static final int SPLASH_TIME_OUT=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run()
+            {
+
+                    Intent intent= new Intent(SplashScreen.this, WelcomeActivity.class);
+                    startActivity(intent);
+                    finish();
+
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
