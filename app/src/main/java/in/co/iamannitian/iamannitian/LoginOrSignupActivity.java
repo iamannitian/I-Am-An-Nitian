81d4f468510cba1a30b627686dc36d97b27dd620
himanshu-yadav-1608 <ydv1608@gmail.com>
2020-05-06 23:28:43 +0530

    Did camelCase and changed some configs

--- app/src/main/java/in/co/iamannitian/iamannitian/LoginOrSignupActivity.java
+++ app/src/main/java/in/co/iamannitian/iamannitian/LoginOrSignupActivity.java
@@ -1,27 +1,26 @@
 package in.co.iamannitian.iamannitian;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class loginscreen extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginOrSignupActivity extends AppCompatActivity {
 
     Button btn1;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginscreen);
+        setContentView(R.layout.activity_login_or_signup);
 
-        btn1= (Button)findViewById(R.id.logsignbtn);
+        btn1= findViewById(R.id.logsignbtn);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1= new Intent(loginscreen.this,logindetails.class);
+                Intent intent1= new Intent(LoginOrSignupActivity.this,logindetails.class);
                 startActivity(intent1);
             }
         });
