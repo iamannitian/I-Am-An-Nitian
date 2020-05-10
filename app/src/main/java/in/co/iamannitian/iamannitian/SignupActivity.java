71107d77a5b653259ee9a4a53a6bf5ebab9784bf
BlackHatos <pnssoftwares7@gmail.com>
2020-05-10 19:39:31 +0530

    establishing connection with database

--- app/src/main/java/in/co/iamannitian/iamannitian/SignupActivity.java
+++ app/src/main/java/in/co/iamannitian/iamannitian/SignupActivity.java
@@ -1,79 +1,131 @@
 package in.co.iamannitian.iamannitian;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
-    public static final String emailregex="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
-
+    private EditText email, username, password;
+    private Button click_to_signup;
     private TextView go_to_login;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        username = findViewById(R.id.username);
+        email    = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        click_to_signup = findViewById(R.id.click_to_signup);
         go_to_login = findViewById(R.id.go_to_login);
 
 
-        final EditText username= findViewById(R.id.username);
-        final EditText email= findViewById(R.id.email);
-       // final EditText phone= findViewById(R.id.phone);
-        final EditText pass= findViewById(R.id.pass);
+        //setting auto full property
+//        username.setAutofillHints(View.AUTOFILL_HINT_USERNAME);
+//        email.setAutofillHints(View.AUTOFILL_HINT_EMAIL_ADDRESS);
+  //      password.setAutofillHints(View.AUTOFILL_HINT_PASSWORD);
 
-        Button signbtn= findViewById(R.id.signup);
 
-        signbtn.setOnClickListener(new View.OnClickListener() {
+        click_to_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //setting errors
                 username.setError(null);
                 email.setError(null);
-                pass.setError(null);
-            //    phone.setError(null);
-                if(username.getText().toString().isEmpty()){
-                    username.setError("Required");
-                    return;
-                }
-                if(email.getText().toString().isEmpty()){
-                    email.setError("Required");
-                    return;
-                }
-                if(pass.getText().toString().isEmpty()){
-                    pass.setError("Enter Password");
-                    return;
-                }
-              /*  if(phone.getText().toString().isEmpty()){
-                    phone.setError("Enter phone number");
-                    return;
-                }*/
-                if(!email.getText().toString().matches(emailregex)){
-                    email.setError("enter valid email");
-                    return;
-                }
-               /* if(phone.getText().toString().length()!=10){
-                    phone.setError("enter valid phone number");
-                }*/
+                password.setError(null);
 
-                else {
-                    Intent intent4= new Intent(SignupActivity.this,MainActivity.class);
-                    startActivity(intent4);}
+                //getting credentials on click the sign up button
+                String user_name = username.getText().toString().trim();
+                String user_email = email.getText().toString().trim();
+                String user_password = password.getText().toString().trim();
+
+                //checking user name
+
+                if(user_name.isEmpty()){
+                    username.setError("");
+                    return;
+                }
+
+
+                //checking user email
+
+                if(!user_email.isEmpty())
+                {
+                   //if not empty then check whether valid or not
+                    final String email_regex =
+                            "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
+                            "@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?" +
+                            "(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]" +
+                            "{0,61}[a-zA-Z0-9])?)*$";
+
+                    Pattern p = Pattern.compile(email_regex);
+                    Matcher m = p.matcher(user_email);
+                    if(!m.matches()) //if email is invalid
+                    {
+                        email.setError("invalid email");
+                        return;
+                    }
+
+                }
+                else
+                {
+                    email.setError("");
+                    return;
+                }
+
+                //checking user password
+
+                if(!user_password.isEmpty()){
+                    //if not empty then check password length
+                    if(user_password.length() < 6)
+                    {
+                        password.setError("password is too sort");
+                        return;
+                    }
+                }
+                else
+                {
+                    password.setError("");
+                    return;
+                }
+
+
+                //if every thing is ok then proceed to sign up
+                proceedToSignup(user_name, user_email , user_password);
 
             }
-
-
-
 
 
         });
 
 
+
+        //go to login activity
         go_to_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,5 +134,45 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+  private void proceedToSignup(final String user_name,final String user_email,final String user_password)
+    {
+        String url = "https://www.iamannitian.co.in/imn_app/signup.php";
+        StringRequest sr = new StringRequest(1, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        if(response.equals("1"))
+                        {
+                            email.setText("");
+                            password.setText("");
+                            username.setText("");
+                            Toast.makeText(SignupActivity.this,"Signup Suuccessful!", Toast.LENGTH_LONG).show();
+                        }
+                      else
+                        {
+                            Toast.makeText(SignupActivity.this,"Signup Failed1", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() { //error
+            @Override
+            public void onErrorResponse(VolleyError error) {
+              //if error occurs
+            }
+        }){
+            @Override
+            public Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map =  new HashMap<>();
+                map.put("nameKey", user_name);
+                map.put("emailKey", user_email);
+                map.put("passwordKey", user_password);
+                return map;
+            }
+        };
+
+        RequestQueue rq = Volley.newRequestQueue(SignupActivity.this);
+        rq.add(sr);
     }
 }
