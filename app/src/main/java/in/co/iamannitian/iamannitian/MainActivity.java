ba72a0e86cd4898dd9658b22e2a260947f3064ab
BlackHatos <pnssoftwares7@gmail.com>
2020-05-31 00:45:56 +0530

    side navigation view and bottom navigation view implemented

--- app/src/main/java/in/co/iamannitian/iamannitian/MainActivity.java
+++ app/src/main/java/in/co/iamannitian/iamannitian/MainActivity.java
@@ -8,10 +8,10 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.res.AssetManager;
-import android.graphics.Typeface;
-import android.graphics.fonts.FontFamily;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,25 +19,41 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
+import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
-
+    private NavigationView navigationView;
+    private SharedPreferences sharedPreferences;
+    private BottomNavigationView bottomNavigationView;
+    private View notificationBadge;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navigationView = findViewById(R.id.navigationView);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        sharedPreferences = getSharedPreferences("appData", MODE_PRIVATE);
+
+
         setUpToolbarMenu();
         setUpDrawerMenu();
+        headerUpdate();
+        showBadge();
     }
 
-    //setting up toolbar menu
+    /*=======>>>>>>> Setting up toolbar menu <<<<<<<<<=========*/
     private void setUpToolbarMenu()
     {
         toolbar = findViewById(R.id.toolbar);
@@ -48,26 +64,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.drawable.app_logo);
 
-        TextView title = (TextView) toolbar.getChildAt(0);
+        /* -- change the font family of the toolbar title -- */
+       //TextView title = (TextView) toolbar.getChildAt(0);
 
         //title.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
     }
 
 
-    //setting up drawer menu
+    /*=======>>>>>>> Setting up navigation drawer <<<<<<<<<=========*/
     private  void setUpDrawerMenu()
     {
-      NavigationView navigationView = findViewById(R.id.navigationView);
       navigationView.setNavigationItemSelectedListener(this);
       drawerLayout = findViewById(R.id.drawerLayout);
       ActionBarDrawerToggle  drawerToggle =
               new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open_drawer, R.string.close_drawer);
 
-
       drawerLayout.addDrawerListener(drawerToggle);
-
       drawerToggle.setDrawerArrowDrawable(new HumbergerDrawable(this));
-
       drawerToggle.syncState();
     }
 
@@ -76,47 +89,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.closeDrawer(GravityCompat.START);
     }
 
-    private void openDrawer()
-    {
-        drawerLayout.openDrawer(GravityCompat.START);
-    }
-
-    public void onBackPress()
-    {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START))
-            closeDrawer();
-        else
-            super.onBackPressed();
-    }
-
-
+    /*=======>>>>>>> Navigation Item Click Listener <<<<<<<<<========*/
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
         closeDrawer();
         switch (menuItem.getItemId()) {
-          /*  case R.id.send_notify:
-                startActivity(new Intent(Admin.this,SendNotification.class));
+            case R.id.logout:
+                logout();
                 break;
-
-            case R.id.add_donar:
-                startActivity(new Intent(Admin.this, AddDoner.class));
-                break;
-
-            case R.id.add_member:
-                startActivity(new Intent(Admin.this, AddTeamMember.class));
-                break;
-
-            case R.id.book_upload:
-                startActivity(new Intent(Admin.this, UploadBooks.class));
-                break;*/
 
         }
         return true;
-
     }
 
+    /*=========>>>>>>> Setting up overflow menu (when toolbar used as action bar) <<<<<<<<<=========*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -124,6 +112,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         inflater.inflate(R.menu.toolbar_menu, menu);
         return true;
     }
+
+
+    /*=======>>>>>>> Overflow menu item Click listener <<<<<<<<<=========*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
@@ -139,5 +130,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    /*=======>>>>>>> Logout function <<<<<<<<<=========*/
+    void logout()
+    {
+        sharedPreferences.edit().clear().apply();
+        Intent intent = new Intent(MainActivity.this, LoginOrSignupActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK); //finish all previous activities
+        startActivity(intent);
+    }
+
+
+    /*=======>>>>>>> Updating the header in the navigation view <<<<<<<<<=========*/
+    public void headerUpdate()
+    {
+        TextView user_name,nit_name;
+        String name = sharedPreferences.getString("userName","");
+        View headView = navigationView.getHeaderView(0);
+        user_name = headView.findViewById(R.id.user_name);
+        nit_name = headView.findViewById(R.id.nit_name);
+        user_name.setText(name.trim());
+    }
+
+    /*=======>>>>>>> Show notification Badge <<<<<<<<<=========*/
+    public void showBadge()
+    {
+
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
+        BottomNavigationItemView itemView = (BottomNavigationItemView) menuView.getChildAt(4);
+        notificationBadge = LayoutInflater.from(this).inflate(R.layout.notification_badge, menuView,false);
+        itemView.addView(notificationBadge);
+    }
+
 
 }
