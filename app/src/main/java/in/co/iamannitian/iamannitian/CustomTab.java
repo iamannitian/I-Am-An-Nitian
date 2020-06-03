6e8289e5c30886ba236f82279b1721da220fcf1b
Shikhar Srivastava <60563576+shikhar8434@users.noreply.github.com>
2020-06-04 00:50:38 +0530

    Viewpager with tabs added

---
+++ app/src/main/java/in/co/iamannitian/iamannitian/CustomTab.java
@@ -0,0 +1,148 @@
+package in.co.iamannitian.iamannitian;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class CustomTab extends AppCompatActivity {
+
+    //This is our tablayout
+    private TabLayout tabLayout;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+
+    //Fragments
+
+    FeedFragment feedFragment;
+    PostFragment postFragment;
+    BlogsFragment blogsFragment;
+
+    String[] tabTitle = {"FEED", "POSTS", "BLOGS"};
+    int[] unreadCount = {0, 5, 0};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setOffscreenPageLimit(3);
+        setupViewPager(viewPager);
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        try {
+            setupTabIcons();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                viewPager.setCurrentItem(position, false);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        // Associate searchable configuration with the SearchView
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_status:
+                Toast.makeText(this, "Home Status Click", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Home Settings Click", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_with_icon:
+                Intent withicon = new Intent(this, MainActivity.class);
+                startActivity(withicon);
+                finish();
+                return true;
+            case R.id.action_without_icon:
+                Intent withouticon = new Intent(this, MainActivity.class);
+                startActivity(withouticon);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        feedFragment = new FeedFragment();
+        postFragment = new PostFragment();
+        blogsFragment = new BlogsFragment();
+        adapter.addFragment(feedFragment, "FEED");
+        adapter.addFragment(postFragment, "POSTS");
+        adapter.addFragment(blogsFragment, "BLOGS");
+        viewPager.setAdapter(adapter);
+    }
+
+    private View prepareTabView(int pos) {
+        View view = getLayoutInflater().inflate(R.layout.activity_custom_tab, null);
+        TextView tv_title = (TextView) view.findViewById(R.id.tv_title);
+        TextView tv_count = (TextView) view.findViewById(R.id.tv_count);
+        tv_title.setText(tabTitle[pos]);
+        if (unreadCount[pos] > 0) {
+            tv_count.setVisibility(View.VISIBLE);
+            tv_count.setText("" + unreadCount[pos]);
+        } else
+            tv_count.setVisibility(View.GONE);
+
+
+        return view;
+    }
+
+    private void setupTabIcons() {
+
+        for (int i = 0; i < tabTitle.length; i++) {
+            /*TabLayout.Tab tabitem = tabLayout.newTab();
+            tabitem.setCustomView(prepareTabView(i));
+            tabLayout.addTab(tabitem);*/
+
+            tabLayout.getTabAt(i).setCustomView(prepareTabView(i));
+        }
+
+
+    }
+
+}
+
